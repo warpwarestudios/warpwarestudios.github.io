ba47cc080931ae72befe7153624bf5a7cb1eac0a
Kenneth Smith <kwgsmith@gmail.com>
2015-08-21 09:21:21 -0500

    Small changes

--- main.js
+++ main.js
@@ -12,9 +12,9 @@ var population = 1;
 var populationmax = 10;
 
 //storage
-var oxygenstorage = 0;
-var foodstorage = 0;
-var materialstorage = 0;
+var oxygenstorage = 1;
+var foodstorage = 1;
+var materialstorage = 1;
 
 
 //buildings
@@ -24,12 +24,6 @@ var lifesupport = 0; //increases oxygen supplies
 var farms = 0; //increases food supplies
 
 
-
-function cookieClick(number){
-    cookies = cookies + number;
-    document.getElementById("cookies").innerHTML = cookies;
-};
-
 function newDay(){
 	updateEnergy();
 	date = date + 1;
@@ -37,20 +31,9 @@ function newDay(){
 	updateMaterials();
 	updateFood();
 	updatePopulation();
+	updateStorage();
 	document.getElementById("date").innerHTML = date;
 }
-
-function buyCursor(){
-    var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));     //works out the cost of this cursor
-    if(cookies >= cursorCost){                                   //checks that the player can afford the cursor
-        cursors = cursors + 1;                                   //increases number of cursors
-    	cookies = cookies - cursorCost;                          //removes the cookies spent
-        document.getElementById('cursors').innerHTML = cursors;  //updates the number of cursors for the user
-        document.getElementById('cookies').innerHTML = cookies;  //updates the number of cookies for the user
-    };
-    var nextCost = Math.floor(10 * Math.pow(1.1,cursors));       //works out the cost of the next cursor
-    document.getElementById('cursorCost').innerHTML = nextCost;  //updates the cursor cost for the user
-};
 
 function awakenPerson()
 {
@@ -122,7 +105,7 @@ function buyMaterialStorage(){
         materialstorage = materialstorage + 1;                                   
     	materials = materials - mStoreCost;                        
         document.getElementById('materialStorage').innerHTML = materialstorage;  
-       	materialsmax = (1 + materialstorage) * 100;
+       	materialsmax = materialstorage * 100;
 		var materialsTotal = materials + "/" + materialsmax;
 		document.getElementById("materials").innerHTML = materialsTotal;
     };
@@ -137,7 +120,7 @@ function buyOxygenStorage(){
         oxygenstorage = oxygenstorage + 1;                                   
     	materials = materials - oStoreCost;                        
         document.getElementById('oxygenStorage').innerHTML = oxygenstorage;  
-       	oxygenmax = (1 + oxygenstorage) * 100;
+       	oxygenmax = oxygenstorage * 100;
 		var materialsTotal = materials + "/" + materialsmax;
 		document.getElementById("materials").innerHTML = materialsTotal;
     };
@@ -153,7 +136,7 @@ function buyFoodStorage(){
         foodstorage = foodstorage + 1;                                   
     	materials = materials - fStoreCost;                        
         document.getElementById('foodStorage').innerHTML = foodstorage;  
-		foodmax = (1 + foodstorage) * 100;
+		foodmax = foodstorage * 100;
 		var materialsTotal = materials + "/" + materialsmax;
 		document.getElementById("materials").innerHTML = materialsTotal;
     };
@@ -171,7 +154,7 @@ function updateEnergy(){
 }
 function updateOxygen(){
 	oxygen = oxygen - population + lifesupport;
-	oxygenmax = (1 + oxygenstorage) * 100;
+	oxygenmax = oxygenstorage * 100;
 	if(oxygen > oxygenmax)
 	{
 		oxygen = oxygenmax;
@@ -182,7 +165,7 @@ function updateOxygen(){
 }
 function updateMaterials(){
 	materials = materials + miners;
-	materialsmax = (1 + materialstorage) * 100;
+	materialsmax = materialstorage * 100;
 	if(materials > materialsmax)
 	{
 		materials = materialsmax;
@@ -193,7 +176,7 @@ function updateMaterials(){
 }
 function updateFood(){
 	food = food - population + farms;
-    foodmax = (1 + foodstorage) * 100;
+    foodmax = foodstorage * 100;
 	if(food > foodmax)
 	{
 		food = foodmax;
@@ -209,4 +192,5 @@ function updatePopulation(){
 	
 	document.getElementById("population").innerHTML = popTotal;
 }
+
 window.setInterval(function(){newDay();}, 1000);
